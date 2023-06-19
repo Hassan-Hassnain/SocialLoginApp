1b28f9e34d4f68d56eab2ebad1359302ee7913e1
Hassan-Hassnain <58207622+Hassan-Hassnain@users.noreply.github.com>
2023-06-19 11:19:28 -0700

    Avatar Type Updated.

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,12 +1,27 @@
+import {ImageStyle, StyleProp, StyleSheet} from 'react-native';
+
 import {AvatarImageSource} from 'react-native-paper/lib/typescript/src/components/Avatar/AvatarImage';
 import {Avatar as PaperAvatar} from 'react-native-paper';
 import React from 'react';
-import {StyleSheet} from 'react-native';
 
 // import {images} from '~/Assets/Images';
 
-export const Avatar = (image: AvatarImageSource) => {
-  return <PaperAvatar.Image size={120} source={image} style={styles.img} />;
+interface Props {
+  image: AvatarImageSource;
+  size?: 'ProfileImage' | 'IconImage' | number;
+  style?: StyleProp<ImageStyle>;
+}
+
+export const Avatar = ({image, size, style}: Props) => {
+  const imageSize =
+    size === 'IconImage' ? 30 : size === 'ProfileImage' ? 120 : size;
+  return (
+    <PaperAvatar.Image
+      size={imageSize}
+      source={image}
+      style={[styles.img, style]}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
